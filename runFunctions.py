30e24e490103242b20bd92605ca101dcab072817
Felipe Sobral <felipesobral_@hotmail.com>
2022-09-02 18:06:05 -0300

    Atualização

--- runFunctions.py
+++ runFunctions.py
@@ -4,12 +4,12 @@ import sqlalchemy
 from sqlalchemy import create_engine
 
 
-leitura_cabecalho2022()
-leitura_cabecalho2021()
-leitura_cabecalho2020()
+#leitura_cabecalho2022()
+#leitura_cabecalho2021()
+#leitura_cabecalho2020()
 
-#leitura_dfs2020()
-#leitura_dfs2021()
-#leitura_dfs2022()
+leitura_dfs2020()
+leitura_dfs2021()
+leitura_dfs2022()
 
 
